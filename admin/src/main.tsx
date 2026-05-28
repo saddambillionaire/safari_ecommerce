cf930b848337cb341fdd0a8918bb5e0118025a96
Saddam Mugongo <saddammugongo2500@gmail.com>
2026-05-28 02:14:50 +0100

    auth implemented

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')!).render(
+// import ur publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error(
+    "Missing Clerk publishable key. Please set VITE_CLERK_PUBLISHABLE_KEY in your environment variables.",
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
