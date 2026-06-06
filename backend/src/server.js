68b1ef9be95122a5bec937cb6e52ff76a3e293fe
Saddam Mugongo <saddammugongo2500@gmail.com>
2026-06-06 09:20:13 +0100

    review config

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,8 @@ import { inngestClient, functions } from "./config/inngest.js";
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
 import orderRoutes from "./routes/order.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import productRoutes from "./routes/product.route.js";
 
 const app = express();
 
@@ -25,6 +27,9 @@ app.use(
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/product", cartRoutes);
 
 app.use(clerkMiddleware()); // Use Clerk middleware for authentication, it adds auth under the request object, req.auth, which contains the user's authentication information.
 app.get("/api/health", (req, res) => {
